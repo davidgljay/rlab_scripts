f0bf83130931a34631a2073731cd51c192497ac1
David Jay <davidgljay@gmail.com>
2023-11-17 22:35:50 -0800

    Establishing connection w/ Notion.

--- csv_registration_import.py
+++ csv_registration_import.py
@@ -1,34 +1,41 @@
+import sys
+import os
 import pandas as pd
-from notion.client import NotionClient
-
-# Notion API Token
-NOTION_API_TOKEN = 'your-notion-api-token'
-# Notion Database ID for Contacts and Invitations
-CONTACTS_DATABASE_ID = 'your-contacts-database-id'
-INVITATIONS_DATABASE_ID = 'your-invitations-database-id'
+from notion_client import Client
+from dotenv import load_dotenv
+from requests.exceptions import HTTPError
 
 # Read CSV
-csv_path = 'your-file.csv'
+csv_path = sys.argv[1]
 df = pd.read_csv(csv_path)
 
 # Connect to Notion
-client = NotionClient(token_v2=NOTION_API_TOKEN)
-contacts_table = client.get_collection_view(CONTACTS_DATABASE_ID)
-invitations_table = client.get_collection_view(INVITATIONS_DATABASE_ID)
+try:
+    # Connect to Notion
+    client = Client(auth=os.getenv('NOTION_API_TOKEN'))
+    contacts_table = client.get_collection_view(os.getenv('CONTACTS_DATABASE_ID'))
+    invitations_table = client.get_collection_view(os.getenv('CONTACTS_DATABASE_ID'))
+
+    # Your other code ...
+
+except HTTPError as e:
+    print(f"Error connecting to Notion API: {e}")
 
 # Update Notion Tables
 for index, row in df.iterrows():
     email = row['email']
-    
+    print(email)
     # Find Contact Page ID
     contact_page = contacts_table.collection.get_rows(search=email)
     if contact_page:
         contact_page_id = contact_page[0].id
+        print(contact_page_id)
 
         # Find Invitation Page
         invitation_page = invitations_table.collection.get_rows(search=contact_page_id)
         if invitation_page:
             invitation_page_id = invitation_page[0].id
+            print(invitation_page_id)
 
             # Update Status to "Registered"
             invitation_page[0].status = "Registered"
